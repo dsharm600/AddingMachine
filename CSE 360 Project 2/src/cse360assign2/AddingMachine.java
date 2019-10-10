4341d49c9d2be3cad8ca5c678e1d86f5578df591
Dhiraj Sharma <dsharm18@asu.edu>
2019-10-09 22:59:13 -0700

    Second Version of AddingMachine.

--- CSE 360 Project 2/src/cse360assign2/AddingMachine.java
+++ CSE 360 Project 2/src/cse360assign2/AddingMachine.java
@@ -1,50 +1,87 @@
+ /*
+ * Author: Dhiraj Sharma
+ * Class ID: 85141
+ * Assignment #: Assignment#2
+ * Description: This class implements various methods that perform 
+ * subtraction and addition operations and return a string showing
+ * the calculation.
+ */
 package cse360assign2;
 /**
- * Class to calculate using add or subtract
+ * Class to calculate using add or subtract method
+ * and also prints the total and the string to show 
+ * calculations.
+ * 
+ * @param total this integer stores the total value
+ * of calulations.
  */
 public class AddingMachine {
-	/**
-     * int to store the total value
-     */
-	private int total;
+	
+	
+	private int total;		// Int to store the total value
+	private String history; // String to store the history
+	
 	/**
      * Constructor
-     * initialize total to 0
+     * initialize total and history to 0
      */
 	public AddingMachine () {
+		
 		total = 0;  // not needed - included for clarity
+		history = "0"; //initialize history string to value 0.
 	}
+	
 	/**
-     * Gets the total value.
-     * @return the total value;
+     * Gets the current total value.
+     * 
+     * @return the current total value;
      */
 	public int getTotal () {
-		return 0;
+		
+		return total;
 	}
+	
 	/**
      * Adds the given value to string history
+     * and the total.
+     * 
      * @param value a value to add to string
      */
 	public void add (int value) {
 		
+		total += value;				// adds the given value to total 
+		history += " + " + value ;	// updates the history with the given value
 	}
+	
 	/**
-     * Subtracts on the given value from history
+     * Subtracts the given value from string history
+     * and the total.
+     * 
      * @param value a value to subtract from string 
      */
 	public void subtract (int value) {
 		
+		total -= value;				// subtracts the given value from total				
+		history += " - " + value ;	// updates the history with the given value
 	}
+	
 	/**
-     * @return the string;
+	 * This method returns the history of calculations
+	 * performed.
+	 * 
+     * @return 	the string history showing the calculations
      */		
 	public String toString () {
-		return "";
+		
+		return history;
 	}
+	
 	/**
-     * Method to clear the history;
+     * Method to clear the history and total.
      */	
 	public void clear() {
 	
+		total = 0;			// set total and history to initial values.
+		history = "0";
 	}
 }
