d61a73b45a22cb7e4fe575fe6c52d5c8019d7e7c
Dhiraj Sharma <dsharm18@asu.edu>
2019-10-09 21:34:03 -0700

    Initial Checking of Adding machine calc.

---
+++ CSE 360 Project 2/src/cse360assign2/AddingMachine.java
@@ -0,0 +1,50 @@
+package cse360assign2;
+/**
+ * Class to calculate using add or subtract
+ */
+public class AddingMachine {
+	/**
+     * int to store the total value
+     */
+	private int total;
+	/**
+     * Constructor
+     * initialize total to 0
+     */
+	public AddingMachine () {
+		total = 0;  // not needed - included for clarity
+	}
+	/**
+     * Gets the total value.
+     * @return the total value;
+     */
+	public int getTotal () {
+		return 0;
+	}
+	/**
+     * Adds the given value to string history
+     * @param value a value to add to string
+     */
+	public void add (int value) {
+		
+	}
+	/**
+     * Subtracts on the given value from history
+     * @param value a value to subtract from string 
+     */
+	public void subtract (int value) {
+		
+	}
+	/**
+     * @return the string;
+     */		
+	public String toString () {
+		return "";
+	}
+	/**
+     * Method to clear the history;
+     */	
+	public void clear() {
+	
+	}
+}
